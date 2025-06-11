2064cd8e84e4f4a135f8ece2aeba3e5350c536e6
racus-pz <75995491+racus-pz@users.noreply.github.com>
2025-06-11 17:17:28 +0200

    Update analytics.ts
    update GA error

--- src/utils/analytics.ts
+++ src/utils/analytics.ts
@@ -53,11 +53,11 @@ export const initializeAnalytics = (gaTrackingId: string) => {
 
   // Initialize GA
   <!-- Google Tag Manager -->
-  <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+  (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
   new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
   j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
   'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-  })(window,document,'script','dataLayer','GTM-PWHZTCDT');</script>
+  })(window,document,'script','dataLayer','GTM-PWHZTCDT');
   <!-- End Google Tag Manager -->
 };
 
